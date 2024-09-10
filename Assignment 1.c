c4c01b0c452bddd4247ba74b5f622ca8aa4d59b4
harshal chaudhari <chaudhariharshal605@gmail.com>
2024-09-10 15:11:44 +0530

    Assignment 1

---
+++ Assignment 1.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+struct Date{
+	int day;
+	int month;
+	int year;
+};
+void initDate(struct Date* ptrDate); 
+void printDateOnConsole(struct Date* ptrDate); 
+void acceptDateFromConsole(struct Date* ptrDate); 
+int main()
+{
+	int choice=1;
+	struct Date d1;
+	initDate(&d1);
+	while(choice!=0)
+	{
+		printf("\n------------------------------------------------------------------------");
+		printf("\n\tMenu:\n\t1.Set Time\n\t2.Display Time\n\t3.Exit\nEnter Choice : ");
+	    scanf("%d",&choice);
+	    switch(choice)
+	    {
+	    	case 1 :acceptDateFromConsole(&d1);
+	    	        break;
+	        case 2 :printDateOnConsole(&d1);
+	                break;
+	        case 3 :printf("----------------------------------- Exit program -------------------------------------\n");
+	                break;
+	        default:printf("Enter a valid choice !");
+		}
+	}
+	
+}
+
+void initDate(struct Date* ptrDate)
+{
+ 	ptrDate->day=0;
+	ptrDate->month=0;
+	ptrDate->year=0;	
+}
+
+void acceptDateFromConsole(struct Date* ptrDate)
+{
+	printf("------------------------------------------------------------------------\n");
+	printf("Enter Current Date :\n");
+	printf("\tDay : ");
+	scanf("%d",&ptrDate->day);
+	printf("\tMonth : ");
+	scanf("%d",&ptrDate->month);
+	printf("\tYear : ");
+	scanf("%d",&ptrDate->year);
+	if(ptrDate->day<=31 && ptrDate->month<=12 && ptrDate->year==2024){
+		printf("Date and Time updated.\n");
+     }
+    else{
+         printf("Please Enter valid date and time.\n");
+         acceptDateFromConsole(ptrDate);
+	    } 
+}
+
+void printDateOnConsole(struct Date* ptrDate)
+{
+  	printf("------------------------------------------------------------------------\n");
+  	printf("\tCurrent Date : %d/%d/%d\n",ptrDate->day,ptrDate->month,ptrDate->year);
+}
